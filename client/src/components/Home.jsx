1dec75fe803197b2e779fd639e2b1c66c2bdee26
soham <sohamsaha22122002@gmail.com>
2025-02-27 14:33:59 +0530

    Home updated

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -56,9 +56,9 @@ export default function Body() {
           Jadavpur University Department of Information Technology has produced numerous distinguished alumni who have made significant contributions in various fields. Some notable achievements include:
           <ul className="list-disc list-inside mt-2">
             <li>Dr. Ujjawl Dutta, Computer Scientist at Adobe.</li>
-            <li>Dr. B 1, a leading expert in artificial intelligence and machine learning.</li>
-            <li>Mr. C 1, a successful entrepreneur and founder of multiple tech startups.</li>
-            <li>Ms. D 1, a prominent figure in the field of cybersecurity.</li>
+            <li>Mr. Arnab Ghatak, a lead Software Engineer at Google.</li>
+            <li>Mr. Anuran Chakraborty, software engineer II at Amazon.</li>
+            <li>Ms. Sayandip Ghosh, software engineer at Microsoft.</li>
           </ul>
         </p>
         <button
