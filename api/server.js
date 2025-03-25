98e48491d6a085fe426d67b0a323bab39db36476
soham <sohamsaha22122002@gmail.com>
2025-03-25 15:12:06 +0530

    final commit before first deploy

--- api/server.js
+++ api/server.js
@@ -75,5 +75,5 @@ app.get('/profile', (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
